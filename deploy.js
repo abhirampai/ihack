217fce35af83d1a88ec6f17c31926872b633733c
Andriaajai <35872115+Andriaajai@users.noreply.github.com>
2020-02-02 12:54:03 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,17 +1,9 @@
-// This script is used in CI environment to update contracts abis and organizations list
-// It sends request with data to common settings storage in 3 cases
-// 1. Deploy to production; 2. Deploy to staging; 3. Push/merge to master branch;
-
 const constants = require('./constants');
 const orgnaizationAbi = require('./build/contracts/Organization.json').abi;
 const charityEventAbi = require('./build/contracts/CharityEvent.json').abi;
 const incomingDonationAbi = require('./build/contracts/IncomingDonation.json').abi;
 const openCharityTokenAbi = require('./build/contracts/OpenCharityToken.json').abi;
 const request = require('request');
-
-
-// это переменная, чтобы различать пуши в мастер и деплой на прод и стейджинг
-// нужно брать из CI окружения
 const env = 'master';
 console.log(`env: ${env}`);
 
